ecbadaea0e993fd749a9992bf9520e2d3619b58c
Michael Perillo <msp501@nyu.edu>
2022-07-03 22:22:53 -0400

    Add waiting function for sending keys, debugged

--- src/Scripts/schedule_getter_storeforce.py
+++ src/Scripts/schedule_getter_storeforce.py
@@ -1,8 +1,9 @@
 import datetime as dt
-from typing import Tuple
+import os
+import time
+from typing import Tuple, Dict
 from src.config.DRIVER_CACHE_DIRECTORY import CACHE_DIR
 from src.config.LUSH_STORE_FORCE_URL import URL
-from src.config.CREDENTIALS import ARI_USER, ARI_PASS
 from src.Classes.work_shift import WorkShift
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -17,51 +18,69 @@ def parse_workday_shift_string(workday_shift_string: str) -> Tuple[dt.time, dt.t
     return start_time, end_time
 
 
+def send_data(input_box: webdriver, _in: str, timeout: int):
+    count = 0
+    while input_box.get_attribute('value') != _in:
+        if count > timeout:
+            return False
+        input_box.send_keys(_in)
+        count += 1
+        time.sleep(1)
+    return True
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(path=CACHE_DIR).install()))
 
-driver.get(URL)
+def get_schedule_dict_for_user(user_in: str, user_pass: str) -> Dict[str, WorkShift]:
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(path=CACHE_DIR).install()))
+    driver.implicitly_wait(10)
+    driver.get(URL)
 
-inputs_locator = driver.find_element(By.ID, 'login-inputs-container')
-login_btn = driver.find_element(By.TAG_NAME, 'Button')
+    inputs_locator = driver.find_element(By.ID, 'login-inputs-container')
+    login_btn = driver.find_element(By.TAG_NAME, 'Button')
 
-user_in = inputs_locator.find_element(By.CSS_SELECTOR, '[type="text"]')
-pass_in = inputs_locator.find_element(By.CSS_SELECTOR, '[type="password"]')
+    user_in_box = inputs_locator.find_element(By.CSS_SELECTOR, '[type="text"]')
+    pass_in_box = inputs_locator.find_element(By.CSS_SELECTOR, '[type="password"]')
 
-user_in.send_keys(ARI_USER)
-pass_in.send_keys(ARI_PASS)
-login_btn.click()
+    LOGIN_TIMEOUT = 10
+    user_in_box.send_keys(user_in)
+    WebDriverWait(driver, LOGIN_TIMEOUT).until(lambda driver: send_data(user_in_box, user_in, LOGIN_TIMEOUT))
+    pass_in_box.send_keys(user_pass)
+    WebDriverWait(driver, LOGIN_TIMEOUT).until(lambda driver: send_data(pass_in_box, user_pass, LOGIN_TIMEOUT))
+    login_btn.click()
 
-menu_buttons = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.ID, 'buttons'))
-schedule_btn = menu_buttons.find_element(By.CSS_SELECTOR, '[data-bind="click: ScheduleClicked"]')
-schedule_btn.click()
+    menu_buttons = driver.find_element(By.ID, 'buttons')
+    schedule_btn = menu_buttons.find_element(By.CSS_SELECTOR, '[data-bind="click: ScheduleClicked"]')
+    schedule_btn.click()
 
-schedule_calendar = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.ID, 'scheduleContent'))
-month = dt.datetime.strptime(schedule_calendar.find_element(By.CSS_SELECTOR, '[data-bind="text: MonthName"]').text, "%B").month
-today = dt.datetime.today().date()
-assert today.month == month
-week_tbody = schedule_calendar.find_element(By.CSS_SELECTOR, '[data-bind="foreach: ScheduleWeeks"]')
-weeks = week_tbody.find_elements(By.TAG_NAME, 'tr')
+    schedule_calendar = driver.find_element(By.ID, 'scheduleContent')
+    week_tbody = schedule_calendar.find_element(By.CSS_SELECTOR, '[data-bind="foreach: ScheduleWeeks"]')
+    month = dt.datetime.strptime(schedule_calendar.find_element(By.CSS_SELECTOR, '[data-bind="text: MonthName"]').text,
+                                 "%B").month
+    today = dt.datetime.today().date()
+    assert today.month == month
+    weeks = week_tbody.find_elements(By.TAG_NAME, 'tr')
 
-sched_dict = {}
-for week in weeks:
-    days = week.find_elements(By.TAG_NAME, 'td')
-    for index, day in enumerate(days):
-        try:
-            workday_str = day.text
-            if len(workday_str) <= 1:
+    sched_dict = {}
+    for week in weeks:
+        days = week.find_elements(By.TAG_NAME, 'td')
+        for index, day in enumerate(days):
+            try:
+                workday_str = day.text
+                if len(workday_str) <= 1:
+                    continue
+                workday_info = workday_str.split('\n')
+                workday_numeric_day = int(workday_info[0])
+                if workday_numeric_day < today.day:
+                    continue
+                workday_shift_string = workday_info[-1]
+                workday_shift_start, workday_shift_end = parse_workday_shift_string(workday_shift_string)
+                workday_date = dt.datetime(today.year, today.month, workday_numeric_day).date()
+                work_shift = WorkShift(workday_date, workday_shift_start, workday_shift_end)
+                sched_dict[workday_date.strftime('%Y%m%d')] = work_shift
+            except Exception:
                 continue
-            workday_info = workday_str.split('\n')
-            workday_numeric_day = int(workday_info[0])
-            if workday_numeric_day < today.day:
-                continue
-            workday_shift_string = workday_info[-1]
-            workday_shift_start, workday_shift_end = parse_workday_shift_string(workday_shift_string)
-            workday_date = dt.datetime(today.year, today.month, workday_numeric_day).date()
-            work_shift = WorkShift(workday_date, workday_shift_start, workday_shift_end)
-            sched_dict[workday_date.strftime('%Y%m%d')] = work_shift
-        except Exception:
-            continue
+    driver.quit()
+    return sched_dict
 
 
-driver.quit()
+
+
